3321f98d4c2e5888faef72794a07e53c3fc04728
hassan mccutchen <has_bum@yahoo.com>
2018-02-15 23:01:31 +0800

    stupid coaching done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+    @message = params[:question]
+
+    if @message == "I am going to work"
+      @response = "Great"
+    elsif @message.include?  "?"
+      @response = "Silly question, get dressed and go to work!"
+
+    else
+      @response = "I don't care, get dressed and go to work!"
+
+    end
+  end
+end
